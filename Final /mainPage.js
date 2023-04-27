e904c7a83184f5882b635f5a3a5b5e6aa932328c
dcb9250 <dcb9250@nyu.edu>
2023-04-27 09:18:56 -0400

    final upload omg

---
+++ Final /mainPage.js
@@ -0,0 +1,16 @@
+const pageContainers = [...document.querySelectorAll('.page-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+pageContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
